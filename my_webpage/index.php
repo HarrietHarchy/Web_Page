81320c81adff01654325f485c1f960a2da3058c0
HarrietHarchy <harriet@aumaharriet@gmail.comcdcd>
2017-12-20 15:23:41 -0500

    Introduction to how my passion on women and technology intersect

---
+++ my_webpage/index.php
@@ -0,0 +1,346 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    
+    <!-- Title Tag -->
+    <title>Harriet</title>
+    
+<!--
+
+November Template
+
+http://www.templatemo.com/tm-473-november
+
+-->
+    <!-- <<Mobile Viewport Code>> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+            
+    <!-- <<Attched Stylesheets>> -->
+    
+    <link rel="stylesheet" href="css/theme.css" type="text/css" />
+    <link rel="stylesheet" href="css/media.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,600italic,400italic,800,700' rel='stylesheet' type='text/css'>    
+    <link href='https://fonts.googleapis.com/css?family=Oswald:400,700,300' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+
+<!-- \\ Begin Holder \\ -->
+<div class="DesignHolder">
+	<!-- \\ Begin Frame \\ -->
+	<div class="LayoutFrame">
+        <!-- \\ Begin Header \\ -->
+        <header>
+            <div class="Center">
+                <div class="site-logo">
+                	<h1><a href="#">Harriet...Auma</span></a></h1>
+
+                </div>
+               <div id="mobile_sec">
+               <div class="mobile"><i class="fa fa-bars"></i><i class="fa fa-times"></i></div>
+                <div class="menumobile">
+                    <!-- \\ Begin Navigation \\ -->
+                    <nav class="Navigation">
+                        <ul>
+                            <li class="active">                                
+                                <a href="#home"><i class="fa fa-home "></i> Home</a>
+                                <span class="menu-item-bg"></span>
+                            </li>
+                            <li>
+                                <a href="#about"><i class="fa fa-check "></i>About</a>
+                                <span class="menu-item-bg"></span>
+                            </li>
+                            <li>
+                                <a href="#services"><i class="fa fa-wrench "></i>Services</a>
+                                <span class="menu-item-bg"></span>
+                            </li>
+                            <li>
+                                <a href="#contact"><i class="fa fa-comments icon-round "></i> Contact</a>
+                                <span class="menu-item-bg"></span>
+                            </li>
+                        </ul>
+                    </nav>
+                    <!-- // End Navigation // -->
+				</div>
+				</div>
+                <div class="clear"></div>
+            </div>
+        </header>
+        <!-- // End Header // -->
+        <!-- \\ Begin Banner Section \\ -->
+
+        <div class="Banner_sec" id="home">
+
+            <!--  \\ Begin banner Side -->
+            <div class="bannerside">
+
+	            <div class="Center">
+
+
+                        <h3> <?php    date_default_timezone_set('Africa/Nairobi');  echo  date("D M d, Y H:i:s a");?> </h3>
+
+                    <!--  \\ Begin Left Side -->
+                    <div class="leftside">
+                        <h3>Welcome<span>To My Website</span></h3>
+
+                        <p>Harriet Auma is a professional web master, programmer and a databse administrator from JKUAT University.</p>
+                        <a href="#about">MORE DETAILS</a>
+                    </div>
+                    <!--  // End Left Side // -->
+                    <!--  \\ Begin Right Side -->
+                    <div class="rightside">
+                   
+                    	    <li>
+                    	        <div class="Slider">
+                    	       
+                                    <figure>
+                                        <img src="img/Harchy.jpg" alt="image">
+                                    </figure>
+                                    <div class="text">
+                                        <div class="Icon">
+                                            <ul>
+                                                <li><a href="#"><i class=""></i></a></li>
+                                                <li><a href="#"><i class=""></i></a></li>
+                                            </ul>	
+                                        </div>                        								
+                                        <div class="Lorem">
+                                            <p><span></span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            
+                            </li>
+                    
+							
+					
+                    		<li>
+                                <div class="Slider">
+                                    <figure><img src="" alt="image"></figure>
+                                    <div class="text">
+                                        
+                                        <div class="Lorem">
+                                            <p><span></span></p>
+                                        </div>
+                                    </div>
+                                </div>
+							</li>
+
+                        <li>
+                                <div class="Slider">
+                                    <figure><img src="img/4.jpg" alt="image"></figure>
+                                    <div class="text">
+                                        <div class="Icon">
+                                            
+                                                
+                                        </div>
+                                        <div class="Lorem">
+                                            <p><span></span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                        </li>
+                   
+            	        
+                    <!--  // End Right Side // -->
+	           </div>
+            <!--  // End banner Side // -->
+            <div class="clear"></div>
+        </div>
+        <!-- // End Banner Section // -->
+         <div class="bgcolor"></div>
+        <!-- \\ Begin Container \\ -->
+        <div id="Container">
+            <!-- \\ Begin About Section \\ -->
+            <div class="About_sec" id="about">
+                <div class="Center">            	
+                    <h2>about us</h2>            		
+                    <p>Harriet is passionate about using technology to bring revolutionary impact to the society around her and the world over.Her hope and vision is to empower people and especially women to take up career in tech and this is where her two passions intersect: women$tech</p>
+                    <div class="Line"></div>
+                    <!-- \\ Begin Tab side \\ -->
+                    <div class="Tabside">
+                        <ul>
+                            <li><a href="javascript:;" class="tabLink activeLink" id="cont-1">Mision</a></li>
+                            <li><a href="javascript:;" class="tabLink" id="cont-2">vision</a></li>
+                            <li><a href="javascript:;" class="tabLink" id="cont-3">Sponsors</a></li>
+                        </ul>
+                      <div class="clear"></div>
+                        <div class="tabcontent" id="cont-1-1">
+                            <div class="TabImage">
+                                <div class="img1">
+                                    <figure><img src="img/2.jpg" alt="image"></figure>
+                                </div>
+                                <div class="img2">
+                                    <figure><img src="img/1.jpg" alt="image"></figure>
+                                </div>
+                            </div>
+                            <div class="Description">
+                                <h3><span></span></h3>
+                                <p></p>
+                                <p></p>
+                            </div>
+                        </div>
+                        <div class="tabcontent hide" id="cont-2-1">
+                            <div class="TabImage">
+                                <div class="img1">
+                                    <figure><img src="img/1.jpg" alt="image"></figure>
+                                </div>
+                                <div class="img2">
+                                    <figure><img src="img/2.jpg" alt="image"></figure>
+                                </div>
+                            </div>
+                            <div class="Description">
+                                <h3>Vision attracts resources<span>Follow your passion and money will follow you</span></h3>
+                                <p></p>
+
+                            </div>
+                        </div>
+                        <div class="tabcontent hide" id="cont-3-1">
+                            <div class="TabImage">
+                                <div class="img1">
+                                    <figure><img src="img/2.jpg" alt="image"></figure>
+                                </div>
+                                <div class="img2">
+                                    <figure><img src="img/1.jpg" alt="image"></figure>
+                                </div>
+                            </div>
+                            <div class="Description">
+                                <h3>We have a team of dedicated sponsors <span>Below is the full list of ou able sponsors</span></h3>
+                                <p>Gulf radio which is the base sponsor, Mr. Agira's family which always supports me either through thick or thin.</p>
+                                <p>Well wishers more so friends and relatives who are always there for me when am in much need during research.</p>
+                            </div>
+                        </div>
+	                    <div class="clear"></div>	
+                    </div>                    
+                    <!-- // End Tab Side // -->
+                </div>
+            </div>
+            <!-- // End About Section // -->
+        <!-- \\ Begin Services Section \\ -->
+        <div class="Services_sec" id="services">
+            <div class="Center">
+                <h2>My Work</h2>
+                <p> xxxxxxxxxxxx</p>
+                <div class="Line"></div>
+                <!-- \\ Begin Services Side  \\ -->
+                <div class="Serviceside">
+                    <ul>
+	                    <li class="Development"><a href="#services"><h4>WEB DEVELOPMENT&DESIGN</h4></a></li>
+	                    <li class="Concept"><a href="#services"><h4>DATABASE ADMINISTRATION</h4></a></li>
+	                    <li class="System"><a href="#services"><h4>MENTORSHIP PROGRAMME</h4></a></li>
+                    </ul>
+                </div>
+                <!-- // End Services Side // -->
+            </div>                
+        </div>
+        <!-- // End Services Section // -->
+        <!-- \\ Begin Pricing Section \\ -->
+
+        <!-- \\ Begin Contact Section \\ -->
+        <div class="Contact_sec" id="contact">
+            <div class="Contactside">
+                <div class="Center">
+                    <h2><i class="fa fa-pencil small-icons "></i> contact me</h2>
+                    <p>This is the only place where clients get value for their money and therefore looking forward to hear from you, through the contact information provided below.</p>
+                    <div class="Line"></div>
+                </div>
+
+            </div>
+
+
+                
+                <!-- \\ Begin Get Section \\ -->
+                <div class="Get_sec">
+                    <div class="Mid">					
+                        <!-- \\ Begin Left Side \\ -->
+                        <div class="Leftside">
+                            <form method="POST">
+                                <fieldset>
+                                    <p><input type="text" name="name" required="required"  value="" placeholder="NAME" class="field"></p>
+                                    <p><input type="email" name="email" required="required" value="" placeholder="EMAIL" class="field"></p>
+                                    <p><input type="number" name="number" required="required" value="" placeholder="PHONE" class="field"></p>
+                                    <p><textarea name="message" cols="2"  required="required" rows="2" placeholder="MESSAGE"></textarea></p>
+                                    <p><input type="submit" name="submit" value="send" class="button"></p>
+                                </fieldset>
+                                <?php
+mysql_connect("localhost","root","") or die("mysql connection failed");
+mysql_select_db("personal") or die("database does not exist");
+                            if(isset($_POST['submit']))  {
+                             $fullname = $_POST['name'];
+                             $email = $_POST['email'];
+                             $number = $_POST['number'];
+                             $message = $_POST ['message'];
+
+                             $query = "INSERT INTO agira(name,email,phone,message) VALUES('$fullname','$email','$number','$message')";
+                             if(mysql_query($query)){
+                                 echo "<div class='alert alert-success'>Successfully submitted $fullname, we'll get back to u soon!</div>";
+                             }
+                             else{
+                                 echo"<div class='alert alert-danger'>error encountered please try again</div>";
+                             }
+
+                             }
+
+                    ?>
+                            </form>
+                        </div>
+                        <!-- // End Left Side // -->
+                        <!-- \\ Begin Right Side \\ -->
+                        <div class="Rightside">
+                            <h3><i class="fa fa-comments small-icons "></i> Get in touch !</h3>
+                                <address>
+                                    75397-00200,Nairobi Kenya.
+                                </address>
+                                <address class="Number">
+                                    (+254) 721103912 , (+254) 738241048
+                                </address>	
+                                <address class="Email">
+                                    <a href="mailto:aumaharriet@gmail.com">aumaharriet@gmail.com</a>
+                                </address>	
+                                <div class="clear"></div>
+                                <ul>
+                                    <li><a rel="nofollow" href="http://www.facebook.com/"
+                                   target="_parent"><img src="img/facebook-icn.png" alt="image"></a></li>
+                                    <li><a href="https://twitter.com/"><img src="img/twitter-icn.png" alt="image"></a></li>
+                                    <li><a href="https://calvinceagira@gmail.com/"><img src="img/google-plus-icn.png" alt="image"></a></li>
+                                </ul>
+                        </div>
+                        <!-- // End Right Side // -->
+                    </div>
+                    <!-- \\ Begin Footer \\-->
+                    <footer>
+                        <div class="Cntr">                
+                            <p>COPYRIGHT © 2016 HARRIET_HARCHY. DESIGN: <a rel="nofollow" href="http://www.aumaharriet@gmail.com.com" target="_parent">BLESSED</a></p>
+                        </div>
+                    </footer>
+                    <!-- // End Footer // -->
+                </div>
+                <!-- // End Get Section // -->
+            
+            </div>
+            <!-- // End Contact Section // -->
+        </div>
+        <!-- // End Container // -->
+	</div>
+	<!-- // End Layout Frame // -->
+</div>
+<!-- // End Design Holder // -->
+<div id="loader-wrapper">
+<div id="loader"></div>
+
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
+
+</div>
+
+<!-- <<Attched Javascripts>> -->
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.sudoSlider.min.js"></script>
+<script type="text/javascript" src="js/global.js"></script>
+<script type="text/javascript" src="js/modernizr.js"></script>
+
+</body>
+</html>
+
